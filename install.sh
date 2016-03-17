95d04b8e7efe68fd10adab93e9855f33de753905
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2016-03-17 11:43:22 +0100

    Automate VBox guest additions upgrade
    Signed-off-by: Olivier Tilmans <olivier.tilmans@uclouvain.be>

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ MINVER="1.5"
 VAGRANTVER=$(vagrant -v | awk '/Vagrant/ { print $NF }')
 CMP=$(echo -e "$MINVER\n$VAGRANTVER" | sort -V | head -n 1)
 
+vagrant plugin install vagrant-vbguest
+
 if [ "$MINVER" != "$CMP" ]; then
     BOXNAME=$(awk -F "=" '/config.vm.box/ { gsub(/ *"*/, "", $NF); print $NF }' Vagrantfile)
     echo "Detected Vagrant version < 1.5 ($VAGRANTVER), manually importing the base box $BOXNAME"
